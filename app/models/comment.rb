e1340296f8790125fb5f624ab9bcf38d1300a8e4
jr-cast <castanos@pm.me>
2022-11-12 14:04:29 -0600

    Delete comments and update :comments_counter after destroy

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,8 +5,13 @@ class Comment < ApplicationRecord
   validates :text, presence: true, length: { in: 1..250 }
 
   after_create :update_comment_counter
+  after_destroy :decrement_comments_counter
 
   def update_comment_counter
     post.increment!(:comments_counter)    
+  end
+
+  def decrement_comments_counter
+    post.decrement!(:comments_counter)
   end
 end
