f05969a4749dd084bea6e2ba00eef3a922ebb15d
jr-cast <castanos@pm.me>
2022-11-20 15:23:05 -0600

    Add position-absolute to devise msgs

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,5 +4,7 @@ class LikesController < ApplicationController
     new_like.user_id = current_user.id
     new_like.post_id = params[:post_id]
     flash[:notice] = new_like.errors.full_messages.to_sentence unless new_like.save
+    @post = Post.find(params[:post_id])
+    redirect_to user_post_path(@post.user_id, @post.id)
   end
 end
