73639a93ec3d2ba3005efc8675e96b3af9585176
jr-cast <castanos@pm.me>
2022-11-29 16:31:54 -0600

    Update host for action.mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,8 +1,9 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  config.hosts << "https://rough-tree-4596.fly.dev"
   # Settings specified here will take precedence over those in config/application.rb.
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'https://rough-tree-4596.fly.dev/' }
   config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
 
